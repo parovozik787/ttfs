00dbc44aa31481cdfc3c3aae4f2a479088b35a16
Pavlo Kovalenko <45419378+parovozik787@users.noreply.github.com>
2020-07-12 18:17:19 +0300

    Feat: header (#1)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,11 @@
 export const Header = () => {
-  console.log("Header");
+  document
+    .querySelector("nav")
+    .addEventListener("click", function ({ target: t }) {
+      if (t.classList.contains("nav__link")) {
+        this.querySelectorAll(".nav__link").forEach((n) =>
+          n.classList.toggle("nav__link--active", n === t)
+        );
+      }
+    });
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import "./Home.scss";
-// import { Header } from "../../components/Header/Header";
+import { Header } from "../../components/Header/Header";
 // import { Footer } from "../../components/Footer/Footer";
 //
-// Header();
+Header();
 // Footer();
